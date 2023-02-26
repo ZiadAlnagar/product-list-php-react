0eb172166873f766b12767cef225580ae8ad141e
ZiadAlnagar <ziadalnagdev@gmail.com>
2023-02-26 05:41:57 +0200

    style: client

--- client/src/components/utils/Input.js
+++ client/src/components/utils/Input.js
@@ -14,21 +14,21 @@ const Input = ({
   children,
   ...rest
 }) => (
-    <label className='field flex col' htmlFor={id}>
-      <div className='input-label'>{label}</div>
-      <input
-        id={id}
-        name={name ?? id}
-        type={type}
-        value={value}
-        onChange={onChange}
-        onBlur={onBlur}
-        placeholder={placeholder}
-        {...rest}
-      />
-      <InputError message={errMsg} />
-      {children}
-    </label>
-  );
+  <label className='field flex col' htmlFor={id}>
+    <div className='input-label'>{label}</div>
+    <input
+      id={id}
+      name={name ?? id}
+      type={type}
+      value={value}
+      onChange={onChange}
+      onBlur={onBlur}
+      placeholder={placeholder}
+      {...rest}
+    />
+    <InputError message={errMsg} />
+    {children}
+  </label>
+);
 
 export default Input;
